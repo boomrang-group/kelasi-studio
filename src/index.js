4c35208d07f14a44a1538e031e1ce77f01adfa40
contact <contact@boomrang-group.com>
2025-06-04 19:46:29 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Assuming you have an App.js or App.jsx component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
